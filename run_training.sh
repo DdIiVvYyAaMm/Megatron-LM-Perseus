c7e834236d705293a42fae9c497c49a07d24b18d
DdIiVvYyAaMm <divyams@umich.edu>
2025-04-22 14:32:26 -0400

    Updating monkey patch usage and custom run_training.sh for running the patch

--- run_training.sh
+++ run_training.sh
@@ -9,12 +9,13 @@ WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 # Paths for saving checkpoints and tokenizer files.
-CHECKPOINT_PATH=/workspace/megatron-lm/experiments/codeparrot-small
-VOCAB_FILE=/workspace/megatron-lm/vocab.json
-MERGE_FILE=/workspace/megatron-lm/merges.txt
-DATA_PATH=/workspace/megatron-lm/codeparrot_content_document
+CHECKPOINT_PATH=/workspace/Megatron-LM/experiments/codeparrot-small
+VOCAB_FILE=/workspace/Megatron-LM/vocab.json
+MERGE_FILE=/workspace/Megatron-LM/merges.txt
+DATA_PATH=/workspace/Megatron-LM/codeparrot_content_document
 
 # Model and training hyperparameters.
+# original train-iters: 150000,  lr-deccay-iters: 150000,  lr-warmup-iters: 2000, save-interval: 2000, eval-interval: 200
 GPT_ARGS="--num-layers 12 \
 --hidden-size 768 \
 --num-attention-heads 12 \
@@ -23,16 +24,16 @@ GPT_ARGS="--num-layers 12 \
 --micro-batch-size 12 \
 --global-batch-size 192 \
 --lr 0.0005 \
---train-iters 150000 \
---lr-decay-iters 150000 \
+--train-iters 250 \
+--lr-decay-iters 250 \
 --lr-decay-style cosine \
---lr-warmup-iters 2000 \
+--lr-warmup-iters 20 \
 --weight-decay 0.1 \
 --adam-beta2 0.999 \
 --fp16 \
 --log-interval 10 \
---save-interval 2000 \
---eval-interval 200 \
+--save-interval 100 \
+--eval-interval 100 \
 --eval-iters 10"
 
 TENSORBOARD_ARGS="--tensorboard-dir /workspace/megatron-lm/experiments/tensorboard"
@@ -41,10 +42,10 @@ TENSORBOARD_ARGS="--tensorboard-dir /workspace/megatron-lm/experiments/tensorboa
 python3 -m torch.distributed.launch $DISTRIBUTED_ARGS \
         pretrain_gpt.py \
         --tensor-model-parallel-size 1 \
-        --pipeline-model-parallel-size 2 \
+        --pipeline-model-parallel-size 1 \
         $GPT_ARGS \
-        --vocab-file /workspace/megatron-lm/vocab.json \
-        --merge-file /workspace/megatron-lm/merges.txt \
+        --vocab-file /workspace/Megatron-LM/vocab.json \
+        --merge-file /workspace/Megatron-LM/merges.txt \
         --save $CHECKPOINT_PATH \
         --load $CHECKPOINT_PATH \
         --data-path $DATA_PATH \
